613548932a05007e98368bcb31574dee06eedd63
neha <neharosma200@gmail.com>
2020-01-24 15:04:20 +0700

    commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About Me</title>
+    <link rel="stylesheet" href="{{url('assets/css/bootstrap.min.css')}}" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    
+  
+</head>
+<body class="bg-dark">
+<div class="container">
+<div class="jumbotron">
+  <div class="row">
+  <div class="col-md-12">
+  <center><h2>About Me</h2></center>
+  <p><center><img src="{{url('assets/img/1.jpg')}}" class="rounded-circle"></center></p>
+  <p>
+    Nama saya Neha Rosma Malini, biasa dipanggil Neha. Saya lahir di Batu Tanggal 25 Februari 2003
+  </p>
+
+        <div class="alert alert-dark" role="alert">
+        <table>
+        <tr>
+            <td> Nama </td>
+            <td>:</td> 
+            <td>Neha Rosma Malini</td>
+        </tr>
+        <tr>
+            <td> Tempat,tanggal lahir</td>
+            <td> : </td>
+            <td>Batu, 25 Februari 2003</td>
+        </tr>
+        <tr>
+            <td>Alamat</td>
+            <td>: </td>
+            <td>Jl. Sakura Giripurno Kec Bumiaji Kota Batu</td>
+        </tr>
+        <tr>
+            <td>Email </td>
+            <td>: </td>
+            <td>neharosma200@gmail.com</td>
+        </tr>
+        <tr>
+            <td>No Telp </td>
+            <td>: </td>
+            <td>085231656361</td>
+            </tr>
+        <tr>
+            <td>Sekolah</td> 
+            <td>: </td>
+            <td>Smk Islam Batu</td>
+        </tr>
+        <tr>
+            <td>Jenis Kelamin </td>
+            <td>:</td>
+            <td>Perempuan</td>
+        </tr>
+        </table>
+        </div>
+
+
+        <a class="btn btn-primary" href="https://www.facebook.com/neha.r.98871" target="_blank" role="button">facebook</a>
+        <a class="btn btn-danger" href="https://www.instagram.com/n_ha_a/?hl=id" target="_blank" role="button">instagram</a>
+  </div>
+  </div>
+</div>
+</div>
+
+</body>
+</html>
